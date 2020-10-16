52e66455a7a0c74115535a9bd42b1d23497177e1
Matthew Rho <mcr9754@rit.edu>
2020-10-15 22:08:17 -0400

    make basic queries

--- dbconnect.py
+++ dbconnect.py
@@ -1,5 +1,9 @@
 import sqlalchemy
+from sqlalchemy import text
 DATABASE = "postgresql://p320_13:aiyohleiCahc2xahtee1@p320_13:5432/reddwarf"
 ENGINE = sqlalchemy.create_engine(DATABASE)
+CONNECTION = ENGINE.connect()
 
-
+def execute_query(query, **kwargs):
+    statement = text(query)
+    return CONNECTION.execute(query, kwargs)

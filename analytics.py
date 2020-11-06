f0a11fea3755a89babe06941de8c31052dee9038
cxs7700 <cxs7700@g.rit.edu>
2020-11-06 17:33:50 -0500

    skeleton

---
+++ analytics.py
@@ -0,0 +1,23 @@
+import dbconnect as db
+import queries as q
+
+def most_popular_recipes():
+  pass
+
+def most_popular_ingredients():
+  pass
+
+def most_recipes_made():
+  pass
+
+def recipes_based_on_storage():
+  pass
+
+def recommendations():
+  pass
+
+def run_analytics():
+  pass
+
+if __name__ == "__main__":
+  run_analytics()
